385ccc96edd6014ae0c2bd4265e03140b1440a4b
Justin Le <justin@jle.im>
2019-10-01 14:54:15 -0700

    robust path on amc

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('child_process').exec;
+const fs = require('fs');
+
+installdir = process.env.HOME + "./local/bin"
 
 try {
   ro = core.getInput('runs-on').split("-")[0];
 
+  fs.mkdirSync(installdir, { recursive: true });
+
   switch(ro) {
     case "ubuntu":
-      dlcommand = "curl -L https://get.haskellstack.org/stable/linux-x86_64.tar.gz | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'";
+      dlcommand = "curl -L https://get.haskellstack.org/stable/linux-x86_64.tar.gz | tar xz --wildcards --strip-components=1 -C " + installdir + " '*/stack'";
       break;
     case "macOS":
-      dlcommand = "curl --insecure -L https://get.haskellstack.org/stable/osx-x86_64.tar.gz | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin";
+      dlcommand = "curl --insecure -L https://get.haskellstack.org/stable/osx-x86_64.tar.gz | tar xz --strip-components=1 --include '*/stack' -C " + installdir;
       break;
     default:
       core.setFailed("Unsopported OS");
@@ -18,7 +23,7 @@ try {
 
   if (typeof dlcommand !== 'undefined') {
     exec(dlcommand, (err, stdout, stderr) => {
-      core.addPath(process.env.HOME + "/.local/bin");
+      core.addPath(installdir);
     });
   }
 } catch (error) {
