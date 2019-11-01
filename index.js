56b1904e9e3c9ec37790641ea7767e485ba6c3b9
Justin Le <justin@jle.im>
2019-11-01 14:56:35 -0700

    fix requirement on run-on

--- index.js
+++ index.js
@@ -2,31 +2,30 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const child_process = require('child_process');
 const fs = require('fs');
+const os = require('os');
 
-installdir = process.env.HOME + "/.local/bin"
+const installdir = process.env.HOME + "/.local/bin";
 
 try {
-  ro = core.getInput('runs-on').split("-")[0];
-
   fs.mkdirSync(installdir, { recursive: true });
 
-  switch(ro) {
-    case "ubuntu":
+  var dlcommand;
+
+  switch(os.platform()) {
+    case "linux":
       dlcommand = "curl -L https://get.haskellstack.org/stable/linux-x86_64.tar.gz | tar xz --wildcards --strip-components=1 -C " + installdir + " '*/stack'";
       break;
-    case "macOS":
+    case "darwin":
       dlcommand = "curl --insecure -L https://get.haskellstack.org/stable/osx-x86_64.tar.gz | tar xz --strip-components=1 --include '*/stack' -C " + installdir;
       break;
     default:
       core.setFailed("Unsopported OS");
   }
 
-  if (typeof dlcommand !== 'undefined') {
+  if (dlcommand) {
     child_process.execSync(dlcommand);
     core.addPath(installdir);
-    console.log("stack update");
     child_process.execSync(installdir + "/stack", ["update"]);
-    console.log("stack update complete");
   }
 } catch (error) {
     core.setFailed(error.message);
